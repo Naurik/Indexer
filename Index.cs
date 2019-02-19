a1c83dcff86022b369ab5488e0432e719e3a4ce7
Nauryzbai <nauzik@mail.ru>
2019-02-19 14:10:26 +0600

    Indexer

---
+++ Index.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Index
+    {
+        private int[] array;
+        public int Size { get; set; }
+
+        public Index(int size)
+        {
+            Size = size;
+            array = new int[size];
+        }
+
+        public int this[int i]
+        {
+            get { return array[i]; }
+            set { array[i] = (int)Math.Pow(value, 2); }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int Size = int.Parse(Console.ReadLine());
+            Index array = new Index(Size);
+            try
+            {
+                    for (int i = 0; i < array.Size; i++)
+                    {
+                        Console.WriteLine("Введите число: ");
+                        int number = int.Parse(Console.ReadLine());
+                        array[i] = number;
+                    }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            for(int i=0; i< array.Size; i++)
+            {
+                Console.WriteLine(i+1 + " элемент массива = " + array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
